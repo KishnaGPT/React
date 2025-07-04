1418fc7f7fb2302d98467adb450d91b2b4678c8d
Krishna Gupta <gkrish3108@gmail.com>
2025-07-04 19:47:54 +0530

    State

---
+++ third_app/src/App.js
@@ -0,0 +1,10 @@
+
+import { AvengerClass } from "./shield";
+
+export function Shieldpass(){
+  return <>
+    <AvengerClass name="Tony Stark" heroic_name="Iron Man"/>
+    <AvengerClass name="Steve Roger" heroic_name="Captain"/>
+    <AvengerClass name="Peter Parker" heroic_name="Spider Man"/>
+  </>
+}
