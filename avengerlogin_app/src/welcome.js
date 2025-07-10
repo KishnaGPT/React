38cd11195c24ae0fdafc231c1f65da045dfbf037
Krishna Gupta <gkrish3108@gmail.com>
2025-07-10 20:47:18 +0530

    Create welcome.js

---
+++ avengerlogin_app/src/welcome.js
@@ -0,0 +1,10 @@
+export function AvengerLogin(props){
+    return <>
+    <h1>Welcome To The <q>Shield</q></h1>
+    <button type="button" onClick={props.clickdetail}>Captain Login</button>
+    <button type="button" onClick={props.clickdetail}>IronMan Login</button>
+    <button type="button" onClick={props.clickdetail}>Thor Login</button>
+    <button type="button" onClick={props.clickdetail}>Sign In</button>
+    </>
+
+}
