90d7958e1cad3e2b9fcaa7d4d982c96956f67e73
Krishna Gupta <gkrish3108@gmail.com>
2025-07-11 11:40:29 +0530

    Create index.js

---
+++ changestate_app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Shieldpass } from './App'
+
+const conatiner = document.getElementById("root");
+const root = ReactDOM.createRoot(conatiner);
+
+root.render(<Shieldpass/>);
