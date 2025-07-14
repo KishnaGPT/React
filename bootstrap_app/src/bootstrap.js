9e4d34d52f79c34dad1b41086216fb406728628d
Krishna Gupta <gkrish3108@gmail.com>
2025-07-14 14:50:56 +0530

    Create bootstrap.js

---
+++ bootstrap_app/src/bootstrap.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+export function AvengerBoot(){
+    return <>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-danger">Danger</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Primary</button>
+    
+    </>
+}
