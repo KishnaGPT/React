036d1115fc67b2c425a8870ffab884ed57876447
Krishna Gupta <gkrish3108@gmail.com>
2025-07-15 10:39:43 +0530

    Create inline.js

---
+++ bootstrap_app/src/inline.js
@@ -0,0 +1,11 @@
+export function AvengerInline(){
+
+    const color  = {color: "red", backgroundColor: "whitesmoke"}
+
+    const font = {fontSize: "80px"}
+
+    return <>
+        
+        <h1 style={{...color,...font}}>Hello Krishna</h1>
+    </>
+}
