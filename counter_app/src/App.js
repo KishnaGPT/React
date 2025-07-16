9778a682e42959e5ffdf826d116c2f7a1a160347
Krishna Gupta <gkrish3108@gmail.com>
2025-07-16 12:18:48 +0530

    Create App.js

---
+++ counter_app/src/App.js
@@ -0,0 +1,17 @@
+
+// import { Counter } from "./counter";
+
+// export function App(){
+
+//   return <>
+//   <Counter/>
+//   </>
+// }
+
+import { Image } from "./Image";
+
+export function App(){
+  return <>
+      <Image/>
+  </>
+}
