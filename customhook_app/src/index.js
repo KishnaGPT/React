2b6f85184044e7c7a8271fa9ac374ce6020701c3
Krishna Gupta <gkrish3108@gmail.com>
2025-07-18 08:02:15 +0530

    Create index.js

---
+++ customhook_app/src/index.js
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom/client';
+import { ShieldPass } from './App';
+
+
+const container = document.getElementById("root");
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<ShieldPass register={true}/>);
