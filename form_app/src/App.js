d6e3096cb4a3b73d47a71dbc129e0a45ba8016e6
Krishna Gupta <gkrish3108@gmail.com>
2025-07-20 10:27:20 +0530

    Create App.js

---
+++ form_app/src/App.js
@@ -0,0 +1,29 @@
+// import { AvengerControl } from "./ControlComponent";
+// import { MultipleAvengerControl } from "./MultipleComponent"
+
+// export function App(){
+
+//   return<>
+//     {/* <AvengerControl/> */}
+//     <MultipleAvengerControl/>
+//   </>
+// }
+
+
+// import { AvengerUncontrol } from "./UncontrolComponent"
+
+// export function App(){
+
+//   return<>
+//     <AvengerUncontrol/>
+//   </>
+// }
+
+
+import { GetSet } from "./GetsetComponent";
+
+export function App(){
+  return <>
+    <GetSet/>
+  </>
+}
